89bed8bfa26ce4cc99b884ba098654225a09444d
Victor Moraes <vct.aragao@gmail.com>
2023-08-12 20:24:32 -0300

    feat: linting and other stuffs

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TransfeeraLogo from './icons/TransfeeraLogo.vue'
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="logo">
+      <TransfeeraLogo />
+    </div>
+    <div class="router-links">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </nav>
+</template>
